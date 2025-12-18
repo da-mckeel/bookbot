903577baae608438b4a5d23ab9dafd3766bef6a2
da-mckeel <damckeel13@gmail.com>
2025-12-17 23:58:17 -0600

    update main to use sys.argv to provide the path to the book

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from stats import count_words, count_letters, sorted_letters
+import sys
+
+def entry_check():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 def get_book_text():
-    with open("books/frankenstein.txt") as f:
+    with open(sys.argv[1]) as f:
         return f.read()
 
 def print_char_counts(char_list):
@@ -12,12 +18,14 @@ def print_char_counts(char_list):
             print(f"{char}: {num}")
 
 def main():
+    entry_check()
     book_text = get_book_text()
     word_count = count_words(book_text)
     letters = count_letters(book_text)
     char_list = sorted_letters(letters)
+    path = sys.argv[1]
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")

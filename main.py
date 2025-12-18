880ffca282b8a4d8b46d3db14f3ca4d14db9fcc6
da-mckeel <damckeel13@gmail.com>
2025-12-17 23:43:34 -0600

    add print_char_counts function and update main to print alpha character report

--- main.py
+++ main.py
@@ -4,13 +4,24 @@ def get_book_text():
     with open("books/frankenstein.txt") as f:
         return f.read()
 
+def print_char_counts(char_list):
+    for d in char_list:
+        char = d["char"]
+        num = d["num"]
+        if char.isalpha():
+            print(f"{char}: {num}")
+
 def main():
     book_text = get_book_text()
     word_count = count_words(book_text)
-    print(f"Found {word_count} total words")
     letters = count_letters(book_text)
-    # print(letters)
     char_list = sorted_letters(letters)
-    print(char_list)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    print_char_counts(char_list)
+    print("============= END ===============")
 
 main()

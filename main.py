05fa1fc60420dad2707a007c75f81128920c9fe3
da-mckeel <damckeel13@gmail.com>
2025-12-17 22:11:35 -0600

    move count_words function to new stats.py and add import to main

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import count_words
+
 def get_book_text():
     with open("books/frankenstein.txt") as f:
         return f.read()
-
-def count_words(text):
-    words = text.split()
-    return len(words)
 
 def main():
     book_text = get_book_text()
